d1806cc31191568fafc3b24c629214251fa95bbc
chribfru <chribfru@gmail.com>
2025-10-03 16:29:15 +0200

    Tilføjet DateOfBirth til Student + i db

--- Student-Management/Models/Student.cs
+++ Student-Management/Models/Student.cs
@@ -6,6 +6,7 @@ public class Student
     public string FirstName { get; set; }
     public string? MiddleName { get; set; }
     public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public string Email { get; set; }
     public DateTime EnrollmentDate { get; set; }
 }
